3d738c57c532665ed6c570b52ea7e4e28674b6d0
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2024-10-03 19:34:21 +0100

    [ADD] Same function example - Naive solution

--- Section 2 - Big O/addTwoTiming.js
+++ Section 2 - Big O/addTwoTiming.js
@@ -7,3 +7,24 @@ var t1 = performance.now();
 addUpTo(1000000000); // 1 Billion
 var t2 = performance.now();
 console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
