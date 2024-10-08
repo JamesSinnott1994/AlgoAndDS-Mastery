df0c06fcfb79f83dd85353136d014ab02b4f708b
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2024-10-08 19:13:03 +0100

    [ADD] sumZero naive solution

---
+++ Section 5 - Problem Solving Patterns/sumZero_naive.js
@@ -0,0 +1,17 @@
+// Naive Solution
+function sumZero(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		for (let j = i+1; j < arr.length; j++) {
+			if (arr[i] + arr[j] === 0) {
+				return [arr[i], arr[j]];
+			}
+		}
+	}
+}
+
+// Time complexity - O(N^2)
+// Space complexity - O(1)
+
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));
+console.log(sumZero([1, 2, 3]));
+console.log(sumZero([-2, 0, 1, 3]));
