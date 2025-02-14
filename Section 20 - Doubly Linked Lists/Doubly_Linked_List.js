fe111f9a4118ecf21835ad82b1d570d334fa4a52
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2025-02-14 19:46:39 +0000

    [ADD] DLL - Set

--- Section 20 - Doubly Linked Lists/Doubly_Linked_List.js
+++ Section 20 - Doubly Linked Lists/Doubly_Linked_List.js
@@ -91,5 +91,14 @@ class DoublyLinkedList{
         }
         return current;
     }
+
+    set(index, val){
+        var foundNode = this.get(index);
+        if(foundNode != null){
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
 }
 
