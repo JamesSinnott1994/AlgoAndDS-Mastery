cf76cc5b1aa23d8cfdd4691fc73e5e1cb622d395
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2025-03-03 20:40:26 +0000

    [ADD] Tree Traversal - Depth First Search Pre-Order

--- Section 23 - Tree Traversal/binarySearchTree.js
+++ Section 23 - Tree Traversal/binarySearchTree.js
@@ -85,6 +85,17 @@ class BinarySearchTree {
         }
         return data;
     }
+
+    DFSPreOrder(){
+        var data = [];
+        function traverse(node){
+            data.push(node.value);
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
 }
 
 
