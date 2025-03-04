2cd7fe416daad46a0499658f8a90bdd0ecfaaff1
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2025-03-04 12:20:01 +0000

    [ADD] Tree Traversal - Depth First Search Post-Order

--- Section 23 - Tree Traversal/binarySearchTree.js
+++ Section 23 - Tree Traversal/binarySearchTree.js
@@ -96,6 +96,17 @@ class BinarySearchTree {
         traverse(this.root);
         return data;
     }
+
+    DFSPostOrder(){
+        var data = [];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(this.root);
+        return data;
+    }
 }
 
 
